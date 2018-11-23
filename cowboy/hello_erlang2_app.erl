ec8c72be584bd845e49810cf02a547bdbbe1271d
toxicdragoneye <36698043+toxicdragoneye@users.noreply.github.com>
2018-11-23 11:46:04 +0100

    added secure listener (not working)

--- cowboy/hello_erlang2_app.erl
+++ cowboy/hello_erlang2_app.erl
@@ -4,15 +4,23 @@
 -export([start/2]).
 -export([stop/1]).
 
-
+% clear TCP listener + secure TLS listener in comment
+% Clients connecting to Cowboy on the clear listener port are expected to use either HTTP/1.1 or HTTP/2
 start(_Type, _Args) ->
 	 Dispatch = cowboy_router:compile([
         {'_', [{"/", hello_handler, []}]}
     ]),
     {ok, _} = cowboy:start_clear(my_http_listener,
-        [{port, 8080}],
-        #{env => #{dispatch => Dispatch}}
+       [{port, 8080}],
+       #{env => #{dispatch => Dispatch}}
     ),
+    %{ok, _} = cowboy:start_tls(my_http_listener,
+    %    [
+    %        {port, 8443},
+    %        {certfile, "/path/to/certfile"},
+    %        {keyfile, "/path/to/keyfile"}
+    %    ],
+    %    #{env => #{dispatch => Dispatch}}),
     hello_erlang2_sup:start_link().
 
 stop(_State) ->
