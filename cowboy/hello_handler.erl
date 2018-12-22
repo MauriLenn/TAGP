4b83f7ea2319b067f6904ec8fbcb295a027c08a2
toxicdragoneye <36698043+toxicdragoneye@users.noreply.github.com>
2018-12-22 20:41:38 +0100

    added index.html

--- cowboy/hello_handler.erl
+++ cowboy/hello_handler.erl
@@ -4,8 +4,18 @@
 -export([init/2]).
 
 init(Req0, State) ->
-    Teller = teller:get(), 
+    %%Teller = teller:get(), 
     Headers = [#{<<"content-type">> => <<"text/html">>}],
-    Body = ,
+    Body = [<<"<html>
+<head>
+        <meta charset=\"utf-8\">
+        <title>Teller!</title>
+</head>
+<body>
+  <h1>Teller</h1>
+  <p>De tellerwaarde is </p>
+  <p>Herlaad om de nieuwe waarde van de teller te zien.</p>
+</body>
+</html>">>],
     Req = cowboy_req:reply(200, Headers, Body, Req0),
     {ok, Req, State}.
